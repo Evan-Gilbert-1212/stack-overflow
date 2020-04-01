4f9128de541294bd5aa596201e5263336c4d9f44
MatthewSDick <matthew.dick@outlook.com>
2020-04-01 13:54:14 -0400

    Home summary Done w/o style

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Summary from '../components/Summary'
 import './home.scss'
 import axios from 'axios'
 
@@ -40,131 +41,12 @@ export function Home() {
           <button>Month</button>
         </div>
         <hr />
-        {/* top of the map */}
-        <div className="home-question-wrapper">
-          <div className="vote-box">
-            <p>1</p>
-            <p>votes</p>
-          </div>
-          <div className="vote-answer">
-            <p>46</p>
-            <p>answer</p>
-          </div>
-          <div className="vote-views">
-            <p>0</p>
-            <p>views</p>
-          </div>
-          <div className="question-area">
-            <div className="question-area-top">
-              <div className="bounty">+50</div>
-              <p className="question-summary">{question[0].questionText}</p>
-            </div>
-            <div className="question-area-bottom">
-              <div className="tags">
-                <div>openmp</div>
-                <div>llvm</div>
-                <div>llvm-clang</div>
-              </div>
-              <div className="contributor">
-                <p>answered 10 hours ago Karthik Sriram 1</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* bottom of the map */}
-
-        {/* <div class="question-summary narrow" id="question-summary-60693395">
-        <div
-          onclick="window.location.href='/questions/60693395/django-settings-for-tests-of-a-reusable-app'"
-          class="cp"
-        >
-          <div class="votes">
-            <div class="mini-counts">
-              <span title="5 votes">5</span>
-            </div>
-            <div>votes</div>
-          </div>
-
-          <div class="status answered">
-            <div class="mini-counts">
-              <span title="3 answers">3</span>
-            </div>
-            <div>answers</div>
-          </div>
-
-          <div class="views">
-            <div class="mini-counts">
-              <span title="70 views">70</span>
-            </div>
-            <div>views</div>
-          </div>
-        </div>
-        <div class="summary">
-          <div
-            class="bounty-indicator"
-            title="this question has an open bounty worth 50 reputation"
-          >
-            +50
-          </div>
-          <h3>
-            <a
-              href="/questions/60693395/django-settings-for-tests-of-a-reusable-app"
-              class="question-hyperlink"
-            >
-              Django: settings for tests of a reusable app?
-            </a>
-          </h3>
-          <div class="tags t-django t-pycharm t-environment-variables t-django-testing">
-            <a
-              href="/questions/tagged/django"
-              class="post-tag"
-              title="show questions tagged &#39;django&#39;"
-              rel="tag"
-            >
-              django
-            </a>{' '}
-            <a
-              href="/questions/tagged/pycharm"
-              class="post-tag"
-              title="show questions tagged &#39;pycharm&#39;"
-              rel="tag"
-            >
-              pycharm
-            </a>{' '}
-            <a
-              href="/questions/tagged/environment-variables"
-              class="post-tag"
-              title="show questions tagged &#39;environment-variables&#39;"
-              rel="tag"
-            >
-              environment-variables
-            </a>{' '}
-            <a
-              href="/questions/tagged/django-testing"
-              class="post-tag"
-              title="show questions tagged &#39;django-testing&#39;"
-              rel="tag"
-            >
-              django-testing
-            </a>
-          </div>
-          <div class="started">
-            <a
-              href="/questions/60693395/django-settings-for-tests-of-a-reusable-app/?lastactivity"
-              class="started-link"
-            >
-              answered{' '}
-              <span title="2020-03-31 08:01:21Z" class="relativetime">
-                11 hours ago
-              </span>
-            </a>
-            <a href="/users/1714030/daniel-backman">Daniel Backman</a>{' '}
-            <span class="reputation-score" title="reputation score " dir="ltr">
-              3,831
-            </span>
-          </div>
-        </div>
-      </div> */}
+        {/* <div className="home-question-wrapper"> */}
+        {/* <ul className="summary-list"> */}
+        {question.map(question => {
+          return <Summary question={question} />
+        })}
+        {/* </div> */}
       </>
     )
   }
